da87d364f207002aef87c899d3be1207645f4772
Melissa <melissa.layser22@gmail.com>
2022-10-17 21:20:44 -0700

    Add closeModal func for non-visible modal/overlay

--- javascript.js
+++ javascript.js
@@ -223,3 +223,10 @@ function openModal(modal) {
     modal.classList.add('active')
     overlay.classList.add('active')
 }
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove('active')
+    ac();
+}
